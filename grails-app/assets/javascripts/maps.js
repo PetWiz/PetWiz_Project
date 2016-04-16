530a2de61e36572e93acb3fdfd2d22be777c3000
CarlosCogua <cacoguaa@unal.edu.co>
2016-04-16 08:03:12 -0500

    Icon's pack added, testing google maps

--- grails-app/assets/javascripts/maps.js
+++ grails-app/assets/javascripts/maps.js
@@ -1,12 +1,13 @@
 var markers = [];
 var map;
+
 function initMap()
 {
     var mapOptions = {
         center: new google.maps.LatLng(4.6561754,-74.0552325),
         zoom: 10,
         scrollwheel: true,
-        disableDefaultUI: false,
+        disableDefaultUI: false
     };
     map = new google.maps.Map(document.getElementById("map"),mapOptions);
     google.maps.event.addDomListener(window, "resize", function() {
@@ -18,7 +19,10 @@ function initMap()
 }
 
 
-function VetUnal() { lista = new google.maps.LatLng(4.6327687,-74.0828724); addMarker( lista );
+function VetUnal() {
+
+    lista = new google.maps.LatLng(4.6327687,-74.0828724); addMarker( lista)
+
 }
 function VetAF() { lista = new google.maps.LatLng( 4.6287051,-74.088666); addMarker( lista );  }
 
@@ -27,7 +31,8 @@ function MarkVet(){
     clearMarkers();
 }
 
-function GuarAH() { lista = new google.maps.LatLng( 4.6288111,-74.1149309); addMarker( lista ); }
+function GuarAH() { lista = new google.maps.LatLng( 4.6288111,-74.1149309);
+    addMarker( lista ); }
 function GuarGC() { lista = new google.maps.LatLng(4.645365,-74.095275); addMarker(lista); }
 
 function MarkGuar(){
@@ -59,24 +64,45 @@ function MarkTrai(){
     clearMarkers();
 }
 
-function HospUV() { lista = new google.maps.LatLng(4.6940758,-74.0691972); addMarker( lista ); }
-function HospPT() { lista = new google.maps.LatLng(4.7079361,-74.0683807); addMarker(lista); }
+function HospUV() {
+    /*var image2 = {
+        url: 'http://image005.flaticon.com/8/svg/9/9963.svg',
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+    };*/
+    var image = {
+        url: 'https://raw.githubusercontent.com/PetWiz/PetWiz_Project/master/grails-app/assets/images/apple-touch-icon-retina.png',
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(20, 20)
+    };
+    lista = new google.maps.LatLng(4.6940758,-74.0691972);
+    //addMarker( lista,image2 );
+    addMarker( lista,image );
+
+}
+function HospPT() { lista = new google.maps.LatLng(4.7079361,-74.0683807);addMarker(lista); }
 
 function MarkHosp(){
     HospUV();HospPT();
     clearMarkers();
 }
 
+function addMarker(location,image) {
 
-var labels = 'Ace';
-function addMarker(location) {
     var marker = new google.maps.Marker({
-        position: location,
-        label: labels,
-        map: map
+        position:location,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: image
     });
     markers.push(marker);
 }
+
+
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
--- grails-app/views/person/muestra.gsp
+++ grails-app/views/person/muestra.gsp
@@ -1,31 +1,14 @@
-
 <!DOCTYPE html>
-<head>
-    <title>index</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta name="layout" content="login"/>
+<html>
+<body onload="changeImage();">
 
-</head>
-<body>
 
-<main>
-    <div class="parallax-container">
-        <div class="parallax"><asset:image src="login11.jpg"/></div>
-        <br><br><br><br><br><br>
-        <div class="container center">
-            <h1 class="petwiz-font big-text font-white petwiz-opacity"> PetWiz </h1>
-            <div class="row">
-                <h5 class="header font-teal petwiz-font medium-text italic trans-hover petwiz-opacity">Lo mejor para ti y tus mascotas</h5>
-            </div>
-            <div class="row">
-                <!--<div class="fb-login-button",  data-max-rows="1" onlogin="checkLoginState();" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>-->
-                <div class="fb-login-button" data-max-rows="5" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true", scope="public_profile,email,user_birthday,user_hometown", onlogin="checkLoginState(); entrar();"></div><!--,user_birthday,user_hometown-->
-            </div>
-
-        </div>
-    </div>
-</main>
+<script>
+    function changeImage() {
+        var image = document.getElementById('myImage'
+        document.write(image);
+    }
+</script>
 
 </body>
 </html>
-
