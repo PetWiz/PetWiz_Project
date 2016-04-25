a6d222e296fcf92b6edd1a6d96583554583c2d69
erinsteven <errin.prieto@gmail.com>
2016-04-24 19:40:30 -0500

    Pet class modified

--- grails-app/domain/petwiz_project/Pet.groovy
+++ grails-app/domain/petwiz_project/Pet.groovy
@@ -3,18 +3,18 @@ package petwiz_project
 /**
  * Created by Andres on 16/03/2016.
  */
-class Pet {
+class Pet implements Serializable{
 
-    String pet_id;
+
     String pet_type;
     String pet_name;
     String pet_genre;
-    Date pet_age;
+    int pet_age;
 
     static belongsTo = [person:Person]
 
     static constraints = {
-        pet_id(nullable: false,blank:false,unique:true)
+
         pet_type(nullable: false,blank:false)
         pet_name(nullable: false,blank:false)
         pet_genre(nullable: false,blank:false)
